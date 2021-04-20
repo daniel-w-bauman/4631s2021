d27ddb4e7af283072db9035e0e93d6a68f995441
Daniel Bauman <daniel.w.bauman@gmail.com>
2021-04-20 17:21:08 -0400

    Added login page

--- StarvinArtist/app/src/main/java/com/example/starvinartist/MainActivity.java
+++ StarvinArtist/app/src/main/java/com/example/starvinartist/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.starvinartist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
