464422530d336b727a7b1e0f8e6096d540b3d765
Daniel Bauman <daniel.w.bauman@gmail.com>
2021-05-04 19:50:24 -0400

    added mongodb atlas connection

--- StarvinArtistBackend/components/mongoConnection.js
+++ StarvinArtistBackend/components/mongoConnection.js
@@ -1,4 +1,5 @@
-const url = 'mongodb://127.0.0.1:27017';
+//const url = 'mongodb://127.0.0.1:27017';
+const url = 'mongodb+srv://starvinapp:<password>@cluster0.ymx7o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const MongoClient = require('mongodb').MongoClient;
 
 exports.connection = MongoClient.connect(url, {
